5df972c3244c8475f6d85076e3664ce5f9bfddf9
Tatyana Savchuk <tatyanasavchuk@Tatyanas-MacBook-Pro.local>
2022-07-25 14:47:27 +0300

    add changes js

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -1,11 +1,14 @@
 const applyTooltip = (button, tooltip, placement = "bottom-end") => {
-  Popper.createPopper(button, tooltip, {
+  const popper = Popper.createPopper(button, tooltip, {
     placement,
   });
   button.addEventListener("mouseenter", (event) => {
     tooltip.removeAttribute("hidden");
   });
   button.addEventListener("mouseleave", (event) => {
+    tooltip.setAttribute("hidden", "");
+  });
+  tooltip.addEventListener("click", (event) => {
     tooltip.setAttribute("hidden", "");
   });
 };
@@ -20,4 +23,18 @@ applyTooltip(
 applyTooltip(
   document.querySelector("#tooltip-trigger-account"),
   document.querySelector("#tooltip-account")
+);
+applyTooltip(
+  document.querySelector("#tooltip-trigger-currency-mobile-menu"),
+  document.querySelector("#tooltip-currency-mobile-menu"),
+  "bottom-start"
+);
+applyTooltip(
+  document.querySelector("#tooltip-trigger-language-mobile-menu"),
+  document.querySelector("#tooltip-language-mobile-menu"),
+  "bottom-start"
+);
+applyTooltip(
+  document.querySelector("#tooltip-trigger-account-mobile-menu"),
+  document.querySelector("#tooltip-account-mobile-menu")
 );
