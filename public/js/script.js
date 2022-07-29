bc05f84b53c15c87b490d253ad4c12ea50e91e73
Tatyana Savchuk <tatyanasavchuk@Tatyanas-MacBook-Pro.local>
2022-07-29 11:43:30 +0300

    add search value

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,17 @@
 const inputSearch = document.getElementById("inputSearch");
 const searchGlass = document.getElementById("searchGlass");
+const search = document.getElementById("search");
 searchGlass.addEventListener("click", (event) => {
+  console.log("search value: ", search.value);
   const className = inputSearch.getAttribute("class");
   if (className.indexOf("input-serch-hidden") > -1) {
     inputSearch.setAttribute(
       "class",
       className.replace("input-serch-hidden", "")
     );
-    document.getElementById("search").focus();
+    search.focus();
   } else {
     inputSearch.setAttribute("class", `${className} input-serch-hidden`);
+    search.value = "";
   }
 });
