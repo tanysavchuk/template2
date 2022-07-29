0bae58f15bd21481728b5b955ec2e49ac07bf5fc
Tatyana Savchuk <tatyanasavchuk@Tatyanas-MacBook-Pro.local>
2022-07-29 14:22:08 +0300

    add input search mobile menu

--- public/js/menulist.js
+++ public/js/menulist.js
@@ -7,4 +7,24 @@ buttonList.addEventListener("click", (event) => {
     menulist.setAttribute("class", "mobile-menu-hidden");
   }
 });
-console.log("location: ", window.location.pathname);
+const searchMobileMenu = document.getElementById("searchMobileMenu");
+console.log("searchMobileMenu");
+const searchInputMobileMenu = document.getElementById("searchInputMobileMenu");
+console.log("searchInputMobileMenu");
+const searchGlassMobileMenu = document.getElementById("searchGlassMobileMenu");
+console.log("searchGlassMobileMenu");
+
+searchGlassMobileMenu.addEventListener("click", (event) => {
+  console.log("searchInputMobileMenu value: ", searchInputMobileMenu.value);
+  const className = searchMobileMenu.getAttribute("class");
+  if (className.indexOf("overflow-hidden") > -1) {
+    searchMobileMenu.setAttribute(
+      "class",
+      className.replace("overflow-hidden", "")
+    );
+    searchInputMobileMenu.focus();
+  } else {
+    searchMobileMenu.setAttribute("class", `${className} overflow-hidden`);
+    searchInputMobileMenu.value = "";
+  }
+});
